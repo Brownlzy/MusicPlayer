e06a4db20325115c7ada9540dd8863995c0b100f
Brownlzy <73223120+Brownlzy@users.noreply.github.com>
2022-09-04 23:50:53 +0800

    替换已弃用的api

--- app/src/main/java/com/liux/musicplayer/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/liux/musicplayer/ui/settings/SettingsFragment.java
@@ -13,6 +13,10 @@ import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.preference.EditTextPreference;
@@ -27,6 +31,60 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private Preference setMainFolder;
     private EditTextPreference MainFolder;
 
+    ActivityResultLauncher<Intent> gotoAppInfo = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            int resultCode = result.getResultCode();
+            if (resultCode == -1) {
+                if (checkPermission()) switch_permission.setChecked(true);
+            }
+        }
+    });
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    Toast.makeText(getActivity(), R.string.permission_granted, Toast.LENGTH_LONG).show();
+                    switch_permission.setChecked(true);
+                } else {
+                    //Toast.makeText(getActivity(), R.string.permission_not_granted, Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent("/");
+                    ComponentName cm = new ComponentName("com.android.settings", "com.android.settings.applications.InstalledAppDetails");
+                    intent.setComponent(cm);
+                    intent.setAction("android.intent.action.VIEW");
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setData(Uri.parse("package:com.liux.musicplayer"));
+                    gotoAppInfo.launch(intent);
+                }
+            });
+
+    ActivityResultLauncher<Intent> getFolderIntent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            int resultCode = result.getResultCode();
+            if (resultCode == -1) {
+                Intent data = result.getData();
+                Uri uri = data.getData();
+                setNewMainFolder(uri);
+            }
+        }
+
+        private void setNewMainFolder(Uri uri) {
+            Uri docUri = DocumentsContract.buildDocumentUriUsingTree(uri, DocumentsContract.getTreeDocumentId(uri));
+            //TODO：选择特殊文件夹时崩溃
+            String path = getPath(getContext(), docUri);
+            //Toast.makeText(getActivity(), uri.toString()+"\n"+path, Toast.LENGTH_LONG).show();
+            // 获取SharedPreferences对象
+            SharedPreferences sp = getContext().getSharedPreferences("com.liux.musicplayer_preferences", Activity.MODE_PRIVATE);
+            // 获取Editor对象
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("mainFolder", path);
+            editor.apply();
+            MainFolder.setSummary(sp.getString("mainFolder", "/storage/emulated/0/Android/data/com.liux.musicplayer/Music/"));
+            setMainFolder.setSummary(MainFolder.getSummary());
+            //Toast.makeText(getActivity(), sp.getString("mainFolder","---"), Toast.LENGTH_LONG).show();
+        }
+    });
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
@@ -35,7 +93,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         MainFolder = findPreference("mainFolder");
         setMainFolder = findPreference("setMainFolder");
         //debug用
-        //MainFolder.setVisible(false);
+        MainFolder.setVisible(false);
 
         switch_permission.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
@@ -62,10 +120,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         setMainFolder.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(@NonNull Preference preference) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
                 //系统调用Action属性
                 try {
-                    startActivityForResult(intent, 2);
+                    getFolderIntent.launch(new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE));
                 } catch (Exception e) {
                     Toast.makeText(getActivity(), "没有正确打开文件管理器", Toast.LENGTH_SHORT).show();
                 }
@@ -73,6 +130,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             }
         });
     }
+
 
     public boolean checkPermission() {
         if (ContextCompat.checkSelfPermission(
@@ -95,61 +153,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             return true;
         } else {
             // You can directly ask for the permission.
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
             Toast.makeText(getActivity(), R.string.asking_permission, Toast.LENGTH_LONG).show();
             return false;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
-        switch (requestCode) {
-            case 1:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(getActivity(), R.string.permission_granted, Toast.LENGTH_LONG).show();
-                    switch_permission.setChecked(true);
-                } else {
-                    //Toast.makeText(getActivity(), R.string.permission_not_granted, Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent("/");
-                    ComponentName cm = new ComponentName("com.android.settings", "com.android.settings.applications.InstalledAppDetails");
-                    intent.setComponent(cm);
-                    intent.setAction("android.intent.action.VIEW");
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setData(Uri.parse("package:com.liux.musicplayer"));
-                    startActivityForResult(intent, 0);
-
-                }
-                return;
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {//选择文件返回
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == -1) {
-            //Toast.makeText(getActivity(), resultCode, Toast.LENGTH_LONG).show();
-            switch (requestCode) {
-                case 2:
-                    Uri uri = data.getData();
-                    Uri docUri = DocumentsContract.buildDocumentUriUsingTree(uri,
-                            DocumentsContract.getTreeDocumentId(uri));
-                    String path = getPath(getContext(), docUri);
-                    //Toast.makeText(getActivity(), uri.toString()+"\n"+path, Toast.LENGTH_LONG).show();
-                    // 获取SharedPreferences对象
-                    SharedPreferences sp = getContext().getSharedPreferences("com.liux.musicplayer_preferences", Activity.MODE_PRIVATE);
-                    // 获取Editor对象
-                    SharedPreferences.Editor editor = sp.edit();
-                    editor.putString("mainFolder", path);
-                    editor.apply();
-                    MainFolder.setSummary(sp.getString("mainFolder", "/storage/emulated/0/Android/data/com.liux.musicplayer/Music/"));
-                    setMainFolder.setSummary(MainFolder.getSummary());
-                    //Toast.makeText(getActivity(), sp.getString("mainFolder","---"), Toast.LENGTH_LONG).show();
-
-                    break;
-            }
         }
     }
 

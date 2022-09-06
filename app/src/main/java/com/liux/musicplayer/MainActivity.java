719548f374a6ff4d72d1d659c43c7bece7a8ff96
Brownlzy <73223120+Brownlzy@users.noreply.github.com>
2022-09-06 12:08:45 +0800

    进一步完善主页框架

--- app/src/main/java/com/liux/musicplayer/MainActivity.java
+++ app/src/main/java/com/liux/musicplayer/MainActivity.java
@@ -1,8 +1,14 @@
 package com.liux.musicplayer;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,10 +16,14 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.liux.musicplayer.databinding.ActivityMainBinding;
+import com.liux.musicplayer.ui.home.HomeFragment;
+import com.liux.musicplayer.ui.settings.SettingsFragment;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private SeekBar playProgress;
+    private TextView musicPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,8 @@ public class MainActivity extends AppCompatActivity{
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        playProgress = findViewById(R.id.seekBar);
+        musicPlaying = findViewById(R.id.musicPlaying);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -31,5 +43,18 @@ public class MainActivity extends AppCompatActivity{
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getTitle().equals("播放") | item.getTitle().equals(String.valueOf(R.string.title_play))) {
+                    musicPlaying.setVisibility(View.GONE);
+                    playProgress.setVisibility(View.VISIBLE);
+                } else {
+                    musicPlaying.setVisibility(View.VISIBLE);
+                    playProgress.setVisibility(View.GONE);
+                }
+                return NavigationUI.onNavDestinationSelected(item, navController);
+            }
+        });
     }
 }
